407f620e6ed801edd22449542b79b57b0221ad3b
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-02-24 09:58:36 +0530

    Update postRoute.js

--- server/route/postRoute.js
+++ server/route/postRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
-router.post('/post-route' , (req , res)=>{
-    res.status(200)
-})
+router.post('/post-route', (req, res) => {
+    res.status(200).send({ message: "Success" });
+});
 module.exports={router}
