d16705e5fb25fc500ae3b76a9912e632e085a776
ABHAY <abhaybalakrishnan884@gmail.com>
2025-02-27 07:48:01 +0530

    update csp headers

--- client/src/middleware/csp.ts
+++ client/src/middleware/csp.ts
@@ -5,17 +5,17 @@ export function csp() {
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline' https://*.fontshare.com;
-    img-src 'self' blob: data:;
+    script-src 'self' 'nonce-{random_nonce}';
+    style-src 'self' 'nonce-{random_nonce}' https://*.fontshare.com;
+    img-src 'self' blob:;
     font-src 'self' https://*.fontshare.com;
-    object-src 'self' data:;
+    object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    connect-src 'self' ${process.env.NEXT_PUBLIC_BACKEND_URL} wss://*.onrender.com https://*.ipify.org;
+    connect-src 'self' ${process.env.NEXT_PUBLIC_BACKEND_URL} wss://*.onrender.com https://api.ipify.org;
     upgrade-insecure-requests;
-  `
+`
     .replace(/\s{2,}/g, " ")
     .trim();
 
