cbb80be5cc7cc7b2b7179a75e0bbecee4d8a35f3
asil mehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2025-03-09 15:22:17 +0530

    cors origin updated

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ const MAX_CONNECTIONS_PER_IP = 8;
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://privy.abhayyy.tech/",
+    origin: "https://privy.abhayyy.tech",
     methods: ["GET", "POST"],
   },
   connectionStateRecovery: {
