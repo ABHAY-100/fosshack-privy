8554b2f9e933f71dacedc6ceeb7d082ba0458163
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-03-19 19:41:24 +0530

    Update postRoute.js

--- server/route/postRoute.js
+++ server/route/postRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 router.post('/post-route', (req, res) => {
+    console.log(req);
     res.send({ message: "Server Alive!" });
 });
 module.exports={router}
